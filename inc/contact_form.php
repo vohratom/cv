7abdc12a88d95b4ba9562a681693cc3a7e842775
unknown <Administrator@VMCTRION13-64.vmc.com>
2016-01-28 17:54:20 +0000

    Sorted again

--- inc/contact_form.php
+++ inc/contact_form.php
@@ -1,9 +1,9 @@
-<form role="form">
-	<div class="form-group">
-		<input type="email" class="form-control input-lg" placeholder="e-mail" id="email">
-	</div>
-	<div class="form-group">
-		<textarea class="form-control input-lg" rows="5" placeholder="message" id="comment"></textarea>
-	</div>
-  <button type="submit" class="btn btn-success btn-lg">SEND NOW</button>
-</form>
+	<form role="form">
+		<div class="form-group">
+			<input type="email" class="form-control input-lg" placeholder="e-mail" id="email">
+		</div>
+		<div class="form-group">
+			<textarea class="form-control input-lg" rows="5" placeholder="message" id="comment"></textarea>
+		</div>
+		<button type="submit" class="btn btn-success btn-lg">SEND NOW</button>
+	</form>
