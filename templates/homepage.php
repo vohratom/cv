d874e1a31294e6452689993899b9a044b0db3b88
unknown <Administrator@VMCTRION13-64.vmc.com>
2016-01-28 17:44:23 +0000

    Finally sorted correctly

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,88 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
-    <title>Tomas Vohradnik</title>
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet/less" type="text/css" href="css/main.less" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="graph/chartist.min.css">
-    <script src="graph/chartist.min.js"></script>
-    <!--NOT WORKING USING A LINE BELLOW WITH URL // <script src="js/less.js" type="text/javascript"></script>-->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div class="header">
-        <img src="images/avatar.jpg" class="img-circle img-thumbnail">
-    </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 center upper bold">
-          <h1 class="bold">Tomas Vohradnik</h1>
-        </div>
-      </div>
-      
-      <div class="col-lg-12 center upper bold" style="border: 1px solid red;">
-        <div class="ct-chart ct-perfect-fourth">
-        
-        
-          <script>
-            <?php 
-              include('inc/graph.js');
-            ?>    
-          </script>
-      
-
-        </div>
-      </div>
-      
-      
-      
-
-      <div class="container">
-        <div class="row">
-           <div class="col-lg-12 center">
-              <h1 style="color: grey;">Experience</h1><br><br>
-            </div>
-        </div>
-      </div>
-
-      <?php include("left.php") ?>
-        
-      <?php include("right.php") ?>  
-    
-    <br><br>
-
-    <div class="container">
-      <div class="row">
-         <div class="col-lg-12 center">
-            <h1>Contact form</h1>
-
-             <?php 
-              include('inc/contact_form.php');
-            ?>  
-
-          </div>
-      </div>
-    </div>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/input_validator.js"></script>
-    
-  </body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">    
+		<title>Tomas Vohradnik</title>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet/less" type="text/css" href="css/main.less" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="graph/chartist.min.css">
+		<script src="graph/chartist.min.js"></script>
+		<!--NOT WORKING USING A LINE BELLOW WITH URL // <script src="js/less.js" type="text/javascript"></script>-->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="header">
+			<img src="images/avatar.jpg" class="img-circle img-thumbnail">
+		</div>
+		<div class="container border">
+			<div class="row">
+				<div class="col-lg-12 center upper bold">
+					<h1 class="bold">Tomas Vohradnik</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 center upper bold" style="border: 1px solid red;">
+					<div class="ct-chart ct-perfect-fourth">
+						<script>
+							<?php 
+								include('inc/graph.js');
+							?>    
+						</script>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 center">
+					<h1 style="color: grey;">Experience</h1><br><br>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8">
+					<?php
+						include('left.php');
+					?>		
+				</div>
+				<div class="col-lg-4">
+					<?php
+						include('right.php');
+					?>
+				</div>
+			</div>
+			<br><br>
+			<div class="row">
+				<div class="col-lg-12 center">
+					<h1>Contact form</h1>
+					<?php 
+						include('inc/contact_form.php');
+					?>
+				</div>
+			</div>
+			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+			<!-- Include all compiled plugins (below), or include individual files as needed -->
+			<script src="js/bootstrap.min.js"></script>
+			<script src="js/input_validator.js"></script>
+		</div>
+	</body>
 </html>
--- templates/right.php
+++ templates/right.php
@@ -1,164 +1,142 @@
-    <div class="col-lg-4">
-          
-          <div class="panel panel-default">
-            <div class="panel-heading positions upper bold center">
-              <span class="glyphicon glyphicon-check" aria-hidden="true"></span> Technical Skills
-            </div>
-            <div class="panel-body">
-              
-              <p class="bold">HTML</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
-                  100%
-                </div>
-              </div>
-
-              <p class="bold">CSS</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
-                  100%
-                </div>
-              </div>
-
-              <p class="bold">Bootstrap</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-striped active lavender" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
-                  90%
-                </div>
-              </div>
-
-              <p class="bold">GIT</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-striped active blue-jeans" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                  80%
-                </div>
-              </div>
-
-              <p class="bold">SQL</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-striped active pink-rose" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                  80%
-                </div>
-              </div>
-
-              <p class="bold">PHP</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                  40%
-                </div>
-              </div>
-
-              <p class="bold">JavaScript</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                  40%
-                </div>
-              </div>
-
-              <p class="bold">LESS CSS</p>
-              <div class="progress">
-                <div class="progress-bar progress-bar-striped active grass" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                  40%
-                </div>
-              </div>
-
-              <p class="center upper bold">Other</p>
-              
-              <div class="center">
-                <span class="label grapefruit">Sublime</span>
-                <span class="label grapefruit">SourceTree</span>
-                <span class="label grapefruit">Photoshop</span>
-                <span class="label grapefruit">Illustrator</span>
-                <span class="label grapefruit">Balsamiq Wireframing</span>
-                <span class="label grapefruit">MySQL Databases</span>
-                <span class="label grapefruit">APIs</span>
-                <span class="label grapefruit">UTMs</span>
-                <span class="label grapefruit">UI/UX</span>
-                <span class="label grapefruit">OS X</span>
-                <span class="label grapefruit">iOS</span>
-                <span class="label grapefruit">Linux Ubuntu</span>
-                <span class="label grapefruit">Windows</span>
-                <span class="label grapefruit">Android</span>
-                <span class="label grapefruit">Marketing</span>
-                <span class="label grapefruit">Trello</span>
-                <span class="label grapefruit">Wordpress</span>
-              </div>
-
-            </div>
-            <div class="panel-footer center">
-              <a href="https://twitter.com/TomasVohradnik" target="_blank" class="btn btn-social-icon btn-twitter">
-              <span class="fa fa-twitter fa-2x"></span>
-              </a>
-
-              <a href="https://www.facebook.com/tom.vohradnik" target="_blank" class="btn btn-social-icon btn-facebook">
-                <span class="fa fa-facebook fa-2x"></span>
-              </a>
-
-              <a href="https://bitbucket.org/vohratom/" target="_blank" class="btn btn-social-icon btn-bitbucket">
-                <span class="fa fa-bitbucket fa-2x"></span>
-              </a>
-            
-              <a href="https://github.com/vohratom" target="_blank" class="btn btn-social-icon btn-github">
-                <span class="fa fa-github fa-2x"></span>
-              </a>
-
-              <a href="https://uk.linkedin.com/in/tomasvohradnik" target="_blank" class="btn btn-social-icon btn-linkedin">
-                <span class="fa fa-linkedin fa-2x"></span>
-              </a>
-            </div>
-          </div>
-          
-          <div class="panel panel-default">
-            <div class="panel-heading positions upper bold center"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> Interests</div>
-            <div class="panel-body">
-              <a href="www.businessinsider.com">Business Insider</a>
-              <a href="www.codecademy.com">Codecademy</a>
-              <a href="www.livecoding.tv">Livecoding</a>
-            </div>
-          </div>
-
-          <div class="panel panel-default">
-            <div class="panel-heading positions upper bold center"><i class="fa fa-trophy"></i> Awards</div>
-            <div class="panel-body">
-              Ceo finalist
-              Certifikaty
-              Ekurzy
-              Filmovy role
-
-            </div>
-          </div>
-
-          <div class="panel panel-default">
-            <div class="panel-heading positions upper bold center"><span class="glyphicon glyphicon-education" aria-hidden="true"></span> Education</div>
-            <div class="panel-body">
-              <h4>Charles University in Prague</h4>
-                <div class="pull-right">
-                  <img src="images/uk.png" width="50px" height="50px">
-                </div>
-              
-                <i>Faculty of Physical Education and Sport</i><br>
-                <span class="small-text">2011 - 2014</span>
-              
-              <br><br>
-              <p>After 6 semesters of successful studying I voluntarily quit the school.
-              Why?
-              - I decided that school doesn't make me a better man. So I have moved to London to learn english and to get new experience with living and working
-              in international evnironment which corresponds with my life plans.</p>
-              
-              <br>
-
-              <h4>High school of Information Technology</h4>
-                <div class="pull-right">
-                  <img src="images/it.jpg" width="50px">
-                </div>
-                
-                <i>Information Technology</i><br>
-                <span class="small-text">2006 - 2010</span>
-              <p>Great school, I have a strong IT background, thank's for that.</p>
-            </div>
-          </div>
-
-
-        </div>
-      </div>
-    </div>
+	
+		<div class="panel panel-default">
+			<div class="panel-heading positions upper bold center">
+				<span class="glyphicon glyphicon-check" aria-hidden="true"></span><span> </span>Technical Skills
+			</div>
+			<div class="panel-body">
+				<p class="bold">HTML</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+						100%
+					</div>
+				</div>
+				<p class="bold">CSS</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+						100%
+					</div>
+				</div>
+				<p class="bold">Bootstrap</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active lavender" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+						90%
+					</div>
+				</div>
+				<p class="bold">GIT</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active blue-jeans" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+						80%
+					</div>
+				</div>
+				<p class="bold">SQL</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active pink-rose" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+						80%
+					</div>
+				</div>
+				<p class="bold">PHP</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+						40%
+					</div>
+				</div>
+				<p class="bold">JavaScript</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+						40%
+					</div>
+				</div>
+				<p class="bold">LESS CSS</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped active grass" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+						40%
+					</div>
+				</div>
+				<p class="center upper bold">Other</p>
+				<div class="center">
+					<span class="label grapefruit">Sublime</span>
+					<span class="label grapefruit">SourceTree</span>
+					<span class="label grapefruit">Photoshop</span>
+					<span class="label grapefruit">Illustrator</span>
+					<span class="label grapefruit">Balsamiq Wireframing</span>
+					<span class="label grapefruit">MySQL Databases</span>
+					<span class="label grapefruit">APIs</span>
+					<span class="label grapefruit">UTMs</span>
+					<span class="label grapefruit">UI/UX</span>
+					<span class="label grapefruit">OS X</span>
+					<span class="label grapefruit">iOS</span>
+					<span class="label grapefruit">Linux Ubuntu</span>
+					<span class="label grapefruit">Windows</span>
+					<span class="label grapefruit">Android</span>
+					<span class="label grapefruit">Marketing</span>
+					<span class="label grapefruit">Trello</span>
+					<span class="label grapefruit">Wordpress</span>
+				</div>
+			</div>
+			<div class="panel-footer center">
+				<a href="https://twitter.com/TomasVohradnik" target="_blank" class="btn btn-social-icon btn-twitter">
+					<span class="fa fa-twitter fa-2x"></span>
+				</a>
+				<a href="https://www.facebook.com/tom.vohradnik" target="_blank" class="btn btn-social-icon btn-facebook">
+					<span class="fa fa-facebook fa-2x"></span>
+				</a>
+				<a href="https://bitbucket.org/vohratom/" target="_blank" class="btn btn-social-icon btn-bitbucket">
+					<span class="fa fa-bitbucket fa-2x"></span>
+				</a>
+				<a href="https://github.com/vohratom" target="_blank" class="btn btn-social-icon btn-github">
+					<span class="fa fa-github fa-2x"></span>
+				</a>
+				<a href="https://uk.linkedin.com/in/tomasvohradnik" target="_blank" class="btn btn-social-icon btn-linkedin">
+					<span class="fa fa-linkedin fa-2x"></span>
+				</a>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading positions upper bold center"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> Interests</div>
+				<div class="panel-body">
+					<a href="www.businessinsider.com">Business Insider</a>
+					<a href="www.codecademy.com">Codecademy</a>
+					<a href="www.livecoding.tv">Livecoding</a>
+				</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading positions upper bold center"><i class="fa fa-trophy"></i> Awards
+			</div>
+			<div class="panel-body">
+				Ceo finalist
+				Certifikaty
+				Ekurzy
+				Filmovy role
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading positions upper bold center"><span class="glyphicon glyphicon-education" aria-hidden="true"></span> Education</div>
+			<div class="panel-body">
+				<h4>Charles University in Prague</h4>
+				<div class="pull-right">
+					<img src="images/uk.png" width="50px" height="50px">
+				</div>
+				<i>Faculty of Physical Education and Sport</i>
+				<br>
+				<span class="small-text">2011 - 2014</span>
+				<br><br>
+				<p>
+					After 6 semesters of successful studying I voluntarily quit the school.
+					Why?
+					- I decided that school doesn't make me a better man. So I have moved to London to learn english and to get new experience with living and working
+					in international evnironment which corresponds with my life plans.
+				</p>
+				<br> 
+				<p>
+					<h4>High school of Information Technology</h4>
+					<div class="pull-right">
+						<img src="images/it.jpg" width="50px">
+					</div>
+					<i>Information Technology</i>
+					<br>
+					<span class="small-text">2006 - 2010</span>
+					Great school, I have a strong IT background, thank's for that.
+				</p>
+			</div>
+		</div>
+	
