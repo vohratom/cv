0a67da4c71d9162252b41a769a5e441583cdf65b
unknown <Administrator@VMCTRION13-64.vmc.com>
2016-02-04 18:02:16 +0000

    Final v1.0

--- templates/intro.php
+++ templates/intro.php
@@ -7,7 +7,7 @@
 			<p>my name is Tomas, I am currently looking for web development position in London, ideally near to <a href="http://bit.ly/1maqP3a" target="_blank">W22JS</a>.</p>
 			<p>I created this website as a promotion of my coding skills. (<a href="https://github.com/vohratom/cv" target="_blank">view the code</a>)</p>
 			<p class="bold">About me</p>
-			<p>Open-minded, positive attitude, love to learn new things. Mac user with <a href="https://www.sublimetext.com/">sublime</a> as a default text editor for coding.</p>
+			<p>Open-minded, positive attitude, love to learn new things. Mac user with <a href="https://www.sublimetext.com/" target="_blank">sublime</a> as a default text editor for coding.</p>
 			<p class="bold">What am I learning right now?</p>
 			<p>I am currently learning PHP framework <a href="https://nette.org/en/">Nette</a>.</p>
 		</div>
